a71c3addb08a0fdf7e33b09379b0c9bff9c39c87
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-07-01 16:55:49 +0200

    [MIG] stock_available_to_promise_release_dynamic_routing: Migration to 18.0

--- stock_available_to_promise_release_dynamic_routing/__manifest__.py
+++ stock_available_to_promise_release_dynamic_routing/__manifest__.py
@@ -7,7 +7,7 @@
     "maintainers": ["jbaudoux"],
     "website": "https://github.com/OCA/stock-logistics-reservation",
     "category": "Warehouse Management",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["stock_available_to_promise_release", "stock_dynamic_routing"],
     "demo": [],

719c87715338a68d9937aec2ca42aed28cafb4d9
Sébastien Alix <sebastien.alix@camptocamp.com>
2025-11-05 16:59:32 +0100

    Revert "[FIX] stock_available_to_promise_release_carrier_alternative: set preferred routes on moves"
    This reverts commit 6068d4f9165c5e4b29e070b5c0eff2c2a4459e38.

--- stock_available_to_promise_release_carrier_alternative/models/stock_picking.py
+++ stock_available_to_promise_release_carrier_alternative/models/stock_picking.py
@@ -1,6 +1,6 @@
 # Copyright 2025 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-from odoo import Command, models
+from odoo import models
 
 
 class StockPicking(models.Model):
@@ -43,7 +43,6 @@ class StockPicking(models.Model):
                 {
                     "location_id": rule.location_src_id.id,
                     "rule_id": rule.id,
-                    "route_ids": [Command.link(rule.route_id.id)],
                     "picking_type_id": rule.picking_type_id.id,
                     "procure_method": rule.procure_method,
                     "propagate_cancel": rule.propagate_cancel,
